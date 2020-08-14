6effc28812914c730deebaffe43f58e35b17816f
practical-trainer-5867 <misterjcr@aol.com>
2020-08-14 03:37:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function nowServing(katzDeliLine) {
     if (katzDeliLine.length > 0) return `Currently serving ${katzDeliLine.shift()}.`
     else return "There is nobody waiting to be served!"
 }
+
+function currentLine(katzDeliLine) {
+    var stg = `The line is currently: `
+    var newLine = []
+    if (katzDeliLine.length === 0) return "The line is currently empty."
+    else {
+        for (let i = 0; i < katzDeliLine.length; i++) {
+            newLine[i] = `${[i + 1]}. ${katzDeliLine[i]}`
+        }
+        stg += newLine.join(', ')
+        return stg;
+    }
+}
